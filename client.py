f217d460b05cf7974583109b4ef8501b15c41f49
Rodolfo <rluthen@gmail.com>
2022-05-14 13:10:54 -0500

    proyecto v1

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import requests
 
+# Enviar archivo para cifrar, firmar y obetener llaves
 files = {'file': open('file.txt', 'rb')}
-
 r = requests.post('http://localhost:8080',files = files)
 
+# Imprimir llave para verificar firmado
 print(r.text)
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import logging
 import nacl.secret
 import nacl.utils
 import nacl.pwhash
+from nacl.public import PrivateKey, Box
 from nacl.signing import SigningKey
 
 class S(BaseHTTPRequestHandler):
@@ -19,18 +20,38 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
+
+        # generar llaves
+        priv_key = PrivateKey.generate()
+        pub_key = priv_key.public_key
+
+        # escribir archivos de llaves
+        pub_key_file = open("pub.key", "w")
+        pub_key_file.write(str(pub_key))
+        pub_key_file.close()
+        priv_key_file = open("priv.key", "w")
+        priv_key_file.write(str(priv_key))
+        priv_key_file.close()
+
+        # cifrar archivo
         key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
-        box = nacl.secret.SecretBox(key)
+        box = Box(priv_key, pub_key)
         encrypted = box.encrypt(bytes(post_data.decode('utf-8'), 'utf-8'))
 
+        # escribir archivo cifrado
         f = open("encrypted.txt", "w")
+        f.write(str(encrypted.ciphertext))
+        f.close()
 
+        # firmar archivo
         signing_key = SigningKey.generate()
         signed = signing_key.sign(encrypted.ciphertext)
 
-        # f.write(bytes(signed, 'utf-8'))
-        f.write(str(signed))
-        f.close()
+        # escribir archivo firmado
+        fs = open("signed.txt", "w")
+        # fs.write(str(signed.decode('utf-8')))
+        fs.write(str(signed))
+        fs.close()
         
         verify_key = signing_key.verify_key
         verify_key_bytes = verify_key.encode()
@@ -38,8 +59,13 @@ class S(BaseHTTPRequestHandler):
         self._set_response()
         self.wfile.write(verify_key_bytes)
 
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n\nEncrypted Body:\n%s\n\Signed Body:\n%s\nVerify Key:\n%s\n",
-            str(self.path), str(self.headers), post_data.decode('utf-8'), encrypted.ciphertext, signed, verify_key_bytes)
+        # escribir log
+        fs = open("log.log", "a")
+        fs.write("POST envio un archivo para procesamiento")
+        fs.close()
+
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\Public Kye:\n%s\nPrivate Key:\n%s\nEncrypted Body:\n%s\nEncryption Key:\n%s\n\Signed Body:\n%s\nVerify Key:\n%s\n",
+            str(self.path), str(self.headers), post_data.decode('utf-8'), pub_key, priv_key, encrypted.ciphertext, priv_key, signed, verify_key_bytes)
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
